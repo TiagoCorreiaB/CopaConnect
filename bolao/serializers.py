ecffe1ef9ad06ef0db3941f0ef2d23ed3d45cb77
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-07-08 23:29:23 -0300

    feat: adicionando validações para bolão

--- bolao/serializers.py
+++ bolao/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Bolao, Palpite
+from django.utils import timezone
 from gemini_api.client import get_descricao_bolao
 
 class BolaoModelSerializer(serializers.ModelSerializer):
@@ -11,6 +12,27 @@ class BolaoModelSerializer(serializers.ModelSerializer):
 
     def get_quantidade_usuarios(self, obj):
         return obj.usuarios.count()
+    
+    def validate_partida(self, value):
+        if value.data < timezone.now():
+            raise serializers.ValidationError(
+                'A partida já aconteceu, então não é possível criar um bolão'
+            )
+        return value
+    
+    def validate_vencedor(self, value):
+        if self.instance is None and value is not None:
+            raise serializers.ValidationError(
+                'Não é possível definir um vencedor no momento da criação do bolão.'
+            )
+        return value
+    
+    def validate_status(self, value):
+        if self.instance is None and value == Bolao.Status.FINALIZADO:
+            raise serializers.ValidationError(
+                'Um bolão não pode ser criado já com o status finalizado.'
+            )
+        return value
 
     def create(self, validated_data):
         descricao = validated_data.get('descricao', '')
