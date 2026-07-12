ad27fb21feb4cfa8479b23bcd7aeebbc56e6902d
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-07-12 13:23:20 -0300

    feat: adicionando validações no modelviewset e no modelserializer de usuario

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,12 +1,64 @@
-from .models import Usuario, Perfil
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.contrib.auth.hashers import make_password
+from .models import Usuario, Perfil
 
-class UsuarioSerializer(serializers.ModelSerializer):
+class UsuarioWriteModelSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(source='first_name', required=True)
+    sobrenome = serializers.CharField(source='last_name', required=True)
+    usuario = serializers.CharField(source='username')
+    senha = serializers.CharField(source='password', write_only=True)
+
     class Meta:
         model = Usuario
-        exclude = ['password','telefone']
+        fields = ['id','usuario','nome','sobrenome','senha','email', 'telefone']
 
-class PerfilSerializer(serializers.ModelSerializer):
+    def validate_senha(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(list(e.messages))
+        
+        return make_password(value)
+
+    def validate_email(self, value):
+        if self.instance and self.instance.email == value:
+            return value
+
+        if Usuario.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                'Este e-mail já está em uso.'
+            )
+        
+        return value
+    
+    def validate_telefone(self, value):
+        if len(value) < 11:
+            raise serializers.ValidationError(
+                'O número de telefone é inválido.'
+            )
+
+        if self.instance and self.instance.telefone == value:
+            return value
+
+        if Usuario.objects.filter(telefone=value).exists():
+            raise serializers.ValidationError(
+                'Este número já está em uso.'
+            )
+        
+        return value
+
+class UsuarioReadModelSerializer(serializers.ModelSerializer):
+    nome = serializers.ReadOnlyField(source='first_name')
+    sobrenome = serializers.ReadOnlyField(source='last_name')
+    usuario = serializers.ReadOnlyField(source='username')
+
+    class Meta:
+        model = Usuario
+        fields = ['id','usuario','nome','sobrenome']
+
+class PerfilModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Perfil
         fields = '__all__'
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,41 @@
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import PermissionDenied
+from django.db.models import Q
+from django_filters import rest_framework as filters
 from .models import Usuario, Perfil
-from .serializers import UsuarioSerializer, PerfilSerializer
+from .serializers import UsuarioReadModelSerializer, UsuarioWriteModelSerializer, PerfilModelSerializer
+
+class UsuarioFilter(filters.FilterSet):
+    nome = filters.CharFilter(method='filter_por_nome')
+
+    class Meta:
+        model = Usuario
+        fields = ['username'] 
+
+    def filter_por_nome(self, queryset, name, value):
+        return queryset.filter(Q(first_name__icontains=value)|Q(last_name__icontains=value))
+
 
 class UsuarioModelViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
+    filterset_class =  UsuarioFilter
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return UsuarioReadModelSerializer
+        
+        return UsuarioWriteModelSerializer
+    
+    def perform_update(self, serializer):
+        if self.get_object() != self.request.user:
+            raise PermissionDenied('Apenas o usuário pode editar.')
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance != self.request.user:
+            raise PermissionDenied('Apenas o usuário pode apagar.')
+        instance.delete()
 
 class PerfilReadUpdateModelViewSet(mixins.ListModelMixin,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,viewsets.GenericViewSet):
     queryset = Perfil.objects.all()
-    serializer_class = PerfilSerializer
+    serializer_class = PerfilModelSerializer
