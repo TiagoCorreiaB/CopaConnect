04855fd746e3ef314d21fe4f2b89bc357f7ac216
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-07-16 04:08:50 -0300

    fix: corrigindo o dockerfile

--- start.sh
+++ start.sh
@@ -13,9 +13,7 @@ echo "=== Iniciando Celery Worker ==="
 celery -A core worker -l info --concurrency=1 &
 
 echo "=== Iniciando Celery Beat ==="
-celery -A core beat -l info &
+celery -A core beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 echo "=== Iniciando Daphne Server ==="
 exec daphne -b 0.0.0.0 -p 8000 core.asgi:application
-
-start.sh
